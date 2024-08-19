3d77ea7b662199ee423b81b53b557a65a88378df
KK2292 <{job.kohei.kwn@gmail.com}>
2024-08-19 18:50:33 +0900

    環境構築

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import { BrowserRouter, Redirect, Route, Routes } from "react-router-dom";
+import { EditList } from "../pages/EditList";
+import { EditTask } from "../pages/EditTask";
 import { Home } from "../pages/Home";
+import { NewList } from "../pages/NewList";
+import { NewTask } from "../pages/NewTask";
 import { NotFound } from "../pages/NotFound";
 import { SignIn } from "../pages/SignIn";
-import { NewTask } from "../pages/NewTask";
-import { NewList } from "../pages/NewList";
-import { EditTask } from "../pages/EditTask";
 import { SignUp } from "../pages/SignUp";
-import { EditList } from "../pages/EditList";
 
 export const Router = () => {
-  const auth = useSelector((state) => state.auth.isSignIn)
+  const auth = useSelector((state) => state.auth.isSignIn);
 
   return (
     <BrowserRouter>
-      <Switch>
+      <Routes>
         <Route exact path="/signin" component={SignIn} />
         <Route exact path="/signup" component={SignUp} />
         {auth ? (
@@ -23,14 +23,18 @@ export const Router = () => {
             <Route exact path="/" component={Home} />
             <Route exact path="/task/new" component={NewTask} />
             <Route exact path="/list/new" component={NewList} />
-            <Route exact path="/lists/:listId/tasks/:taskId" component={EditTask} />
+            <Route
+              exact
+              path="/lists/:listId/tasks/:taskId"
+              component={EditTask}
+            />
             <Route exact path="/lists/:listId/edit" component={EditList} />
           </>
         ) : (
           <Redirect to="/signin" />
         )}
         <Route component={NotFound} />
-      </Switch>
+      </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
